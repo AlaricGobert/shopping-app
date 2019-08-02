9826ed10eb90cf7fcb940836474f6f084d348a32
Alaric Gobert <alaric.gobert@hotmail.com>
2019-08-02 10:48:41 +0200

    Observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,11 @@
+import { Injectable } from '@angular/core';
+
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -16,7 +16,7 @@ export class RecipeService {
                 new Ingredient('Meat', 1),
                 new Ingredient('French Fries', 20)
             ]),
-            
+
         new Recipe(
             'Big Fat Burger',
             'What else you need to say ?',
@@ -27,12 +27,16 @@ export class RecipeService {
             ])
       ];
 
-    constructor(private slService: ShoppingListService){
-        
+    constructor(private slService: ShoppingListService) {
+
     }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
 
     addIngredientToShoppingList(ingredients: Ingredient[]) {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
+import { Subject } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
-   
+    ingredientsChanged = new Subject<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -14,16 +15,16 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
-    addIngredients(ingredients: Ingredient[]){
+    addIngredients(ingredients: Ingredient[]) {
         // for (let ingredient of ingredients) {
         //     this.addIngredient(ingredient);
         // }
 
-        //Depuis ES06
+        // Depuis ES06
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients);
+        this.ingredientsChanged.next(this.ingredients);
     }
 }
